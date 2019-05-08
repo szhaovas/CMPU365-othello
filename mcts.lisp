069f2e51fd3036c4132cdf1db608313ec43b6ee7
Zhao Shihan <szhao@vassar.edu>
2019-05-08 17:20:48 -0400

    2019-05-08.17:20:48

--- mcts.lisp
+++ mcts.lisp
@@ -320,8 +320,7 @@
          (mc-scores (mc-node-veck-scores nodey))
          (amaf-visits (mc-node-amaf-visits nodey))
          (amaf-scores (mc-node-amaf-scores nodey)))
-        (format t "~A~%" mv-index)
-        (format t "~A~%" result)
+        (format t "~A~%" (last move-acc 4))
         ;; increment MC stats
         (incf (mc-node-num-visits nodey))
         (incf (svref mc-visits mv-index))
